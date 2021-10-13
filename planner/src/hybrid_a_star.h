9063ba8b902f5702b42c8d98bd5873bfab53d8c2
Louis Filipozzi <louis.filipozzi@gmail.com>
2021-10-12 19:01:44 -0700

    Precompute heuristic with non holonomic constraints without obstacles in hybrid A*

--- planner/src/hybrid_a_star.h
+++ planner/src/hybrid_a_star.h
@@ -38,11 +38,6 @@ namespace Planner {
 				return path->GetFinalState();
 			}
 
-			const Ref<Path<Pose2D<>>>& GetPath() const
-			{
-				return path;
-			}
-
 			bool operator==(const State& rhs) const
 			{
 				return discrete == rhs.discrete;
@@ -229,43 +224,102 @@ namespace Planner {
 
 		class NonHolonomicHeuristic : public AStarHeuristic<State> {
 		public:
-			NonHolonomicHeuristic(const Ref<StateSpace>& stateSpace) :
-				m_stateSpace(stateSpace) { }
+			// FIXME Should not be public, look at https://stackoverflow.com/questions/8147027/how-do-i-call-stdmake-shared-on-a-class-with-only-protected-or-private-const
+			NonHolonomicHeuristic(double spatialResolution, double angularResolution, double minTurningRadius, 
+				double reverseCostMultiplier, double forwardCostMultiplier, double directionSwitchingCost,
+				unsigned int numSpatial) :
+				spatialResolution(spatialResolution), angularResolution(angularResolution), minTurningRadius(minTurningRadius), 
+				reverseCostMultiplier(reverseCostMultiplier), forwardCostMultiplier(forwardCostMultiplier), directionSwitchingCost(directionSwitchingCost),
+				numSpatial(numSpatial), numAngular(ceil(2*M_PI / angularResolution)), offset(floor(numSpatial / 2.0) * spatialResolution)
+			{
+				m_values = new double**[numSpatial];
+				for (unsigned int i = 0; i < numSpatial; ++i) {
+					m_values[i] = new double*[numSpatial];
+					for(unsigned int j = 0; j < numSpatial; j++) {
+						m_values[i][j] = new double[numAngular];
+					}
+				}
+			}
+			~NonHolonomicHeuristic()
+			{
+				for (unsigned int i = 0; i < numSpatial; i++) {
+					for (unsigned int j = 0; j < numSpatial; j++) {
+						delete[] m_values[i][j];
+					}
+					delete[] m_values[i];
+				}
+				delete[] m_values;
+			}
+
+			/**
+			 * @brief Generate the NonHolonomicHeuristic heuristic.
+			 */
+			static Ref<NonHolonomicHeuristic> Build(const Ref<StateSpace>& stateSpace, double spatialSize)
+			{
+				PP_INFO("Generating non-holonomic heuristic without obstacle.");
+
+				const double& spatialResolution = stateSpace->spatialResolution;
+				const double& angularResolution = stateSpace->angularResolution;
+				const double& minTurningRadius = stateSpace->minTurningRadius;
+				const double& reverseCostMultiplier = stateSpace->reverseCostMultiplier;
+				const double&forwardCostMultiplier = stateSpace->forwardCostMultiplier;
+				const double& directionSwitchingCost = stateSpace->directionSwitchingCost;
+
+				unsigned int numSpatialGuess = ceil(spatialSize / stateSpace->spatialResolution);
+				if (numSpatialGuess % 2 == 0)
+					numSpatialGuess++;
+
+				auto heuristic = makeRef<NonHolonomicHeuristic>(
+					spatialResolution, angularResolution, minTurningRadius, 
+					reverseCostMultiplier, forwardCostMultiplier, directionSwitchingCost,
+					numSpatialGuess);
+				double***& values = heuristic->m_values;
+				const double& offset = heuristic->offset;
+				const unsigned int& numSpatial = heuristic->numSpatial;
+				const unsigned int& numAngular = heuristic->numAngular;
+
+				Pose2D<> goal(0.0, 0.0, 0.0);
+				for (int i = 0; i < numSpatial; i++) {
+					for (int j = 0; j < numSpatial; j++) {
+						for (int k = 0; k < numAngular; k++) {
+							Pose2D<> pose(i * spatialResolution - offset, j * spatialResolution - offset, k * angularResolution);
+							values[i][j][k] = stateSpace->ComputeCost(pose, goal);
+						}
+					}
+				}
+
+				PP_INFO("Non-holonomic heuristic without obstacle generated.");
+				return heuristic;
+			}
 
 			virtual double GetHeuristicValue(const State& from, const State& to) override
 			{
-				// TODO this heuristic could be pre-computed offline by setting the goal at the origin
-#if 0 // Heuristic estimates only length
-				double distanceMultiplier = std::min(m_stateSpace->reverseCostMultiplier, m_stateSpace->forwardCostMultiplier);
-				if (distanceMultiplier == 0.0)
-					return 0.0;
-
-				// Euclidean distance
 				auto delta = to.GetPose() - from.GetPose();
-				double euclideanDistance = sqrtf(powf(delta.x, 2) + powf(delta.y, 2));
+				delta.theta = delta.WrapTheta();
 
-				// Distance with Reeds-Shepp path
-				double reedsSheppDistance = m_stateSpace->ComputeDistance(from.GetPose(), to.GetPose());
+				int i = (int)round((delta.x + offset) / spatialResolution);
+				int j = (int)round((delta.y + offset) / spatialResolution);
+				int k = (int)round(delta.theta / angularResolution);
+				if (k == numAngular)
+					k = 0;
 
-				double distance = std::max(euclideanDistance, reedsSheppDistance);
-				return distanceMultiplier * distance;
-#else // Heuristic estimate cost
-				double distanceMultiplier = std::min(m_stateSpace->reverseCostMultiplier, m_stateSpace->forwardCostMultiplier);
+				if (i < 0 || i >= numSpatial || j < 0 || j >= numSpatial) {
+					double euclideanDistance = sqrtf(powf(delta.x, 2) + powf(delta.y, 2));
+					double distanceMultiplier = std::min(reverseCostMultiplier, forwardCostMultiplier);
+					return distanceMultiplier * euclideanDistance;
+				}
 
-				// Euclidean distance
-				auto delta = to.GetPose() - from.GetPose();
-				double euclideanDistance = sqrtf(powf(delta.x, 2) + powf(delta.y, 2));
-
-				// Distance with Reeds-Shepp path
-				double reedsSheppCost = m_stateSpace->ComputeCost(from.GetPose(), to.GetPose());
-
-				double heuristic = std::max(distanceMultiplier * euclideanDistance, reedsSheppCost);
-				return heuristic;
-#endif
+				return m_values[i][j][k];
 			}
 
+		public:
+			const double spatialResolution, angularResolution, minTurningRadius;
+			const double reverseCostMultiplier, forwardCostMultiplier, directionSwitchingCost;
+			const unsigned int numSpatial, numAngular;
+			const double offset;
+
 		private:
-			Ref<StateSpace> m_stateSpace;
+			double*** m_values;
 		};
 
 		class ObstaclesHeuristic : public AStarHeuristic<State> {
@@ -286,9 +340,10 @@ namespace Planner {
 			m_stateSpace(stateSpace), m_stateValidator(stateValidator)
 		{
 			m_heuristic = makeRef<AStarCombinedHeuristic<State>>();
-			m_nonHoloHeuristic = makeRef<NonHolonomicHeuristic>(m_stateSpace);
+			m_nonHoloHeuristic = NonHolonomicHeuristic::Build(m_stateSpace, 100.0);
 			m_obstacleHeuristic = makeRef<ObstaclesHeuristic>();
 			m_heuristic->Add(m_nonHoloHeuristic, m_obstacleHeuristic);
+			
 			m_aStarSearch = makeScope<AStar<State, State::Hash>>(m_stateSpace, m_heuristic);
 		}
 		virtual ~HybridAStar() = default;
